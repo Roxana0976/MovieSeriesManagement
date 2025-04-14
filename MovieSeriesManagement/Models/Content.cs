38decb76c133e1fb2016e29485993b4f2c5e1aa3
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-14 15:13:08 -0600

    Create Content.cs

---
+++ MovieSeriesManagement/Models/Content.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieSeriesManagement.Models
+{
+    public enum ContentType
+    {
+        Movie,
+        Series
+    }
+
+    public class Content
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> Genres { get; set; } = new List<string>();
+        public string Platform { get; set; }
+        public int ReleaseYear { get; set; }
+        public double AverageRating { get; set; }
+        public ContentType Type { get; set; }
+        public int ViewCount { get; set; }
+        public int Duration { get; set; } // In minutes for movies, total minutes for series
+
+        // Specific for Series
+        public List<Season> Seasons { get; set; } = new List<Season>();
+    }
+
+    public class Season
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public List<Episode> Episodes { get; set; } = new List<Episode>();
+    }
+
+    public class Episode
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; } // In minutes
+    }
+}
